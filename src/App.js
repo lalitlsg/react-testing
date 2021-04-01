e53a34b57608dff709fe8f7062e85d747622878a
Lalit Garghate <lalit.garghate@gmail.com>
2021-04-01 17:11:22 +0530

    integration test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "../src/App.css";
 
 export const add = (a, b) => a + b;
 
+export const total = (tax, amt) => `$${add(tax, amt)}`;
+
 const App = () => {
   return <div className="App-header">Learn React Testing</div>;
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App, { add } from "./App";
+import App, { add, total } from "./App";
 
 // test("renders learn react link", () => {
 //   const { getByText } = render(<App />);
@@ -13,4 +13,9 @@ test("add", () => {
   const value = add(2, 3);
   expect(value).toBe(5);
   expect(add(5, 5)).toBe(10);
+});
+
+// integration test
+test("total", () => {
+  expect(total(10, 10)).toBe("$20");
 });
