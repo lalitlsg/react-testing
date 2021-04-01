e4478ade830763c7a317bf3161f5f0e17c33469d
Lalit Garghate <lalit.garghate@gmail.com>
2021-04-01 10:30:28 +0530

    react testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "../src/App.css";
-import Button from "./Components/Button";
 
 export const App = () => {
-  return (
-    <div className="App-header">
-      <Button primary>My button</Button>
-      <Button>Lalit</Button>
-    </div>
-  );
+  return <div className="App-header">Testing</div>;
 };
