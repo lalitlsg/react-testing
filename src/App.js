786620dfd4c1ac85f33d9f653be0f2870df2c4a5
Lalit Garghate <lalit.garghate@gmail.com>
2021-04-02 15:20:40 +0530

    component render testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ export const add = (a, b) => a + b;
 export const total = (tax, amt) => `$${add(tax, amt)}`;
 
 const App = () => {
-  return <div className="App-header">Learn React Testing</div>;
+  return (
+    <div className="App-header">
+      <h1>Learn React Testing</h1>
+      <button id="btn">Click</button>
+    </div>
+  );
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import ReactDOM from "react-dom";
 import App, { add, total } from "./App";
 
 // test("renders learn react link", () => {
@@ -9,13 +9,22 @@ import App, { add, total } from "./App";
 // });
 
 // unit test
-test("add", () => {
-  const value = add(2, 3);
-  expect(value).toBe(5);
-  expect(add(5, 5)).toBe(10);
-});
+// test("add", () => {
+//   const value = add(2, 3);
+//   expect(value).toBe(5);
+//   expect(add(5, 5)).toBe(10);
+// });
 
 // integration test
-test("total", () => {
-  expect(total(10, 10)).toBe("$20");
+// test("total", () => {
+//   expect(total(10, 10)).toBe("$20");
+// });
+
+// component render testing
+
+test("heading should contain", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+  expect(root.querySelector("h1").textContent).toBe("Learn React Testing");
+  expect(root.querySelector("button").id).toBe("btn");
 });
